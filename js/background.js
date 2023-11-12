fea088a9c40a115902b60b15a39b1f2ccd43d2ba
최슬기 <choisk09@gmail.com>
2023-11-12 22:07:43 +0900

    amanded background part

--- js/background.js
+++ js/background.js
@@ -1,7 +1,19 @@
-const body = document.body;
+// const body = document.body;
 
-const random = Math.floor(Math.random() * 4)
+// const random = Math.floor(Math.random() * 4)
 
-body.style.backgroundImage = `url(/img/${random}.jpg)`
-body.style.backgroundSize = 'cover'
-body.style.height = '100vh'
+// body.style.backgroundImage = `url(/img/${random}.jpg)`
+// body.style.backgroundSize = 'cover'
+// body.style.height = '100vh'
+
+const images = [
+    "0.jpg", "1.jpg", "2.jpg", "3.jpg"
+]
+
+const chosenImage = images[Math.floor(Math.random() * images.length)]
+
+const bgImage = document.createElement("img")
+bgImage.src = `img/${chosenImage}`
+
+document.body.appendChild(bgImage)
+bgImage.classList.add("bg-image")
