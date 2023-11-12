17d5d352de2158eafe943ed23b340e2a258f0e61
최슬기 <choisk09@gmail.com>
2023-11-12 20:15:53 +0900

    created clock

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,12 @@
-const clock = document.getElementById('clock');
+const clock = document.querySelector('h2#clock');
 
-const date = new Date();
-const hour = date.getHours();
-const minute = date.getMinutes();
-const second = date.getSeconds();
+function getClock() {
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+    const second = String(date.getSeconds()).padStart(2, '0');
+    clock.innerText = `${hour}:${minute}:${second}`
+}
 
-
+getClock();
+setInterval(getClock, 1000);
