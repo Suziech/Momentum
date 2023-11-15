e8a765dbd54afffb8da07f6bd54d3912f6ff1eef
Suziech <choisk09@gmail.com>
2023-11-15 17:21:01 +0900

    added more beautiful images

--- js/background.js
+++ js/background.js
@@ -7,7 +7,7 @@
 // body.style.height = '100vh'
 
 const images = [
-    "0.jpg", "1.jpg", "2.jpg", "3.jpg"
+    "0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"
 ]
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
