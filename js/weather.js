b5b5655635b8df338cee0e65f44c1b45215cac5c
Suziech <choisk09@gmail.com>
2023-11-15 16:28:45 +0900

    fixed weather operator

--- js/weather.js
+++ js/weather.js
@@ -7,8 +7,10 @@ function onGeoOk(position) {
         const weather = document.querySelector("#weather span:first-child")
         const city = document.querySelector("#weather span:last-child")
         city.innerText = data.name
-        weather.innerHTML = data.weather[0].main === 'Clouds' ? `<i class="fa-solid fa-cloud"></i> <span>${Math.floor(data.main.temp)}°C</span>` : `<i class="fa-solid fa-cloud-showers-heavy"></i> <span>${Math.floor(data.main.temp)}°C</span>`
-        // weather.innerText = `${data.weather[0].main === 'Clouds' ? clouds : sun} ${Math.floor(data.main.temp)}°C`
+        weather.innerHTML = data.weather[0].main === 'Clouds' ? 
+        `<i class="fa-solid fa-cloud"></i> <span>${Math.floor(data.main.temp)}°</span>` 
+        : data.weather[0].main === 'Clear' ? `<i class="fa-regular fa-sun"></i> <span>${Math.floor(data.main.temp)}°</span>`
+        : data.weather[0].main === 'Rain' && `<i class="fa-solid fa-cloud-rain"></i>`
     })
 }
 
